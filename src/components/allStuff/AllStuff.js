1e935b17cade12633687d197878aa84ee401bf2e
Corey Lutrick <coreylutrick@gmail.com>
2018-07-13 19:29:04 -0700

    able to log in and logout, routing to proper paths

---
+++ src/components/allStuff/AllStuff.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './AllStuff.css';
+
+class AllStuff extends React.Component {
+  render () {
+    return (
+      <div className="AllStuff">
+        <div className="AllTheStuff">
+          <div className="menu">
+            <header className="top">
+              <h1>
+                All the Stuff
+              </h1>
+            </header>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AllStuff;
---
+++ src/components/myStuff/MyStuff.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './MyStuff.css';
+
+class MyStuff extends React.Component {
+  render () {
+    return (
+      <div className="MyStuff">
+        <div className="MyTheStuff">
+          <div className="menu">
+            <header className="top">
+              <h1>
+                My Stuff
+              </h1>
+            </header>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MyStuff;
